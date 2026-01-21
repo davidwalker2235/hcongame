d23d6c7b26fa3d6443546e1190f051ad270267c5
davidwalker2235 <davidwalker2235@gmail.com>
2026-01-21 16:31:43 +0100

    fix vercel error

--- app/levels/page.tsx
+++ app/levels/page.tsx
@@ -1,8 +1,21 @@
 'use server';
 
+'use server';
+
+import { Suspense } from "react";
 import { LEVEL_TEXTS } from "../data/levelTexts";
 import { LevelsShell } from "./LevelsShell";
 
 export default async function LevelsPage() {
-  return <LevelsShell levelTexts={LEVEL_TEXTS} />;
+  return (
+    <Suspense
+      fallback={
+        <div style={{ minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <p>Loading...</p>
+        </div>
+      }
+    >
+      <LevelsShell levelTexts={LEVEL_TEXTS} />
+    </Suspense>
+  );
 }
