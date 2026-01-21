0cf264b8082a3313e366037709a89478862587ad
davidwalker2235 <davidwalker2235@gmail.com>
2026-01-21 17:23:04 +0100

    add social media

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -38,13 +38,23 @@ function AboutContent() {
           {userData && (
             <div style={{ marginTop: '20px' }}>
               <p className={styles.text}>
-                <strong>Nickname:</strong> {userData.nickname}
+                <strong>Hi</strong> {userData.nickname}
               </p>
               <p className={styles.text}>
-                <strong>Email:</strong> {userData.email}
+              We are ERNI. Since 1994, we have focused on advising companies on Innovation and Technology, with the aim of boosting their performance through software development and the digitalization of their critical business processes, thus laying the foundations for a digital future.
+
+The key to success is the mindset of our ERNIans, shaped by the values and virtues that define us. We work side by side with our clients to help them become more efficient and innovative than their competitors, forming partnerships based on genuine values to achieve ambitious goals.
               </p>
             </div>
           )}
+          <div style={{ marginTop: '20px' }}>
+            Follow us!
+            <div className={styles.followLinks}>
+              <a href="https://www.linkedin.com/company/erni/posts/?feedView=all" target="_blank" rel="noreferrer">[LinkedIn]</a>
+              <a href="https://www.instagram.com/ernigroup/" target="_blank" rel="noreferrer">[Intagram]</a>
+              <a href="https://www.youtube.com/@erniacademy" target="_blank" rel="noreferrer">[Youtube]</a>
+            </div>
+          </div>
         </div>
       </main>
     </div>
